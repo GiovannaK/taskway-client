648592fa856413bbd3ba54a40baf3ceac16202ad
Giovanna Cunha <giovannakelli@gmail.com>
2021-07-01 18:58:20 -0300

    material ui has been instaled and styled components was configured to work with ssr

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Button from '@material-ui/core/Button'
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        djf
+      </Button>
+    </div>
+  )
+}
