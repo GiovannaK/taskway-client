4c5439d59fb3fcee57fbb2d81c6694d458f31a75
Giovanna Cunha <giovannakelli@gmail.com>
2021-07-29 19:27:32 -0300

    feat: Apollo Provider was configured to support http and ws connection

--- src/services/ApolloClient.js
+++ src/services/ApolloClient.js
@@ -1,10 +1,30 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, split } from '@apollo/client';
 import { createUploadLink } from 'apollo-upload-client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 const link = createUploadLink({ uri: 'http://localhost:4000/graphql', credentials: 'include' });
 
+const wsLink = process.browser ? new WebSocketLink({
+  uri: 'ws://localhost:4000/graphql',
+  options: {
+    reconnect: true,
+  },
+}) : null;
+
+const splitLink = process.browser ? split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition'
+      && definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  link,
+) : link;
 export const client = new ApolloClient({
   ssrMode: typeof window === 'undefined',
-  link,
+  splitLink,
   cache: new InMemoryCache(),
 });
