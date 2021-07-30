573399ff0cfcd1f515bec671bf59e1b6de40b3a9
Giovanna Cunha <giovannakelli@gmail.com>
2021-07-29 21:24:20 -0300

    fix: subscriptions setup now works only in browser

--- src/services/ApolloClient.js
+++ src/services/ApolloClient.js
@@ -3,8 +3,6 @@ import { createUploadLink } from 'apollo-upload-client';
 import { WebSocketLink } from '@apollo/client/link/ws';
 import { getMainDefinition } from '@apollo/client/utilities';
 
-const link = createUploadLink({ uri: 'http://localhost:4000/graphql', credentials: 'include' });
-
 const wsLink = process.browser ? new WebSocketLink({
   uri: 'ws://localhost:4000/graphql',
   options: {
@@ -12,19 +10,19 @@ const wsLink = process.browser ? new WebSocketLink({
   },
 }) : null;
 
+const link = createUploadLink({ uri: 'http://localhost:4000/graphql', credentials: 'include' });
+
 const splitLink = process.browser ? split(
   ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === 'OperationDefinition'
-      && definition.operation === 'subscription'
-    );
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
   },
   wsLink,
   link,
 ) : link;
+
 export const client = new ApolloClient({
   ssrMode: typeof window === 'undefined',
-  splitLink,
+  link: splitLink,
   cache: new InMemoryCache(),
 });
