dc44683afbe04c4056ce76c7cb6de9744b88f890
Giovanna Cunha <giovannakelli@gmail.com>
2021-08-18 01:17:21 -0300

    update breakpoints for tab component

--- src/components/TabComponent/styles.js
+++ src/components/TabComponent/styles.js
@@ -2,11 +2,11 @@ import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
-    [theme.breakpoints.up('md')]: {
+    [theme.breakpoints.up('lg')]: {
       top: '6.6vh',
     },
     [theme.breakpoints.down('md')]: {
-      top: '5vh',
+      top: '4.6vh',
     },
     [theme.breakpoints.down('sm')]: {
       top: '6vh',
